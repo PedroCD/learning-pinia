f09b89789b40d4353ccdca8b2e54787936084275
PedroCD <pedro@buyviu.com>
2023-09-13 08:44:52 +0100

    Subscribing to the state

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,36 @@ import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "@/stores/ProductStore";
 import { useCartStore } from "@/stores/CartStore";
+import {reactive, ref} from "vue";
 
 const cartStore = useCartStore()
 const productStore = useProductStore()
+const history = reactive([])
+const doingHistory = ref(false)
+
+history.push((JSON.stringify(cartStore.$state)))
+
+const undo = () => {
+  if (history.length === 1) return
+  doingHistory.value = true
+  history.pop()
+  cartStore.$state = JSON.parse(history.at(-1))
+  doingHistory.value = false
+}
+
+cartStore.$subscribe((mutation, state) => {
+  if (!doingHistory.value) {
+    history.push(JSON.stringify(state))
+  }
+})
+
 cartStore.$onAction(({ name, store, args, after, onError }) => {
   if (name === 'addItems') {
     after(() => {             // If your action return something, you can add a result to callback parameter
       console.log(args[0]);   // and use it on after or onError
+    })
+    onError((error) => {
+      console.log("Hello error", error.message)
     })
   }
 })
@@ -19,6 +42,9 @@ productStore.fill();
 
 <template>
   <div class="container">
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">Undo</AppButton>
+    </div>
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
